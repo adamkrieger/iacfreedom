11e0686c7eafa8b082b7d6efa1f01664ac95093b
Adam Krieger <akrieger@obsglobal.com>
2019-01-15 16:16:34 -0600

    fixed typo

--- orderservice/tag_push.sh
+++ orderservice/tag_push.sh
@@ -3,7 +3,7 @@
 echo Tagging Image for AWS ECR...
 
 AWS_ACCT_NUM=$(aws sts get-caller-identity --output text --query 'Account')
-docker tag iacf_order_service:$BUILDENVVER $AWS_ACCT_NUM.dkr.ecr.us-east-1.amazonaws.com/iacf_order_service:1.0.0
+docker tag iacf_order_service:1.0.0 $AWS_ACCT_NUM.dkr.ecr.us-east-1.amazonaws.com/iacf_order_service:1.0.0
 
 echo Push Image to AWS ECR Repository...
 
