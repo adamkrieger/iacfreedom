3d2e99c07a5d48b2e02ff61ef4189a57b3d972c5
Adam Krieger <akrieger@obsglobal.com>
2019-01-15 14:56:59 -0600

    removed profile argument

--- orderservice/build.sh
+++ orderservice/build.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env ash
 mkdir -p bin
 
+echo Getting golang Dependencies
+
 go get -u "github.com/gin-gonic/gin"
+
+echo Building Executable
 
 GOOS=linux go build -a -o bin/orderservice main.go
 
-pip --version
+echo Copying Executable to Media Library
 
-aws s3 cp --profile $1 ./bin/orderservice s3://bin-iacfreedom/orderservice/1.0.0/orderservice
+# aws s3 cp --profile $1 ./bin/orderservice s3://bin-iacfreedom/orderservice/1.0.0/orderservice
+aws s3 cp ./bin/orderservice s3://bin-iacfreedom/orderservice/1.0.0/orderservice
 
 # ls -al bin
 
