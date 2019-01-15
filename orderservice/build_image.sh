292c0525f4c0cd111722b80c3f4019dba5ac3934
Adam Krieger <akrieger@obsglobal.com>
2019-01-15 15:16:04 -0600

    attempting to also build docker image withing build env

--- orderservice/build_image.sh
+++ orderservice/build_image.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-aws s3 cp --profile iacbuildtemp s3://bin-iacfreedom/orderservice/1.0.0/orderservice ./bin/orderservice
 
-# ls -al bin
-
-# docker build \
-#     -t iactalk_orderservice \
-#     .
+docker build \
+    -t iactalk_orderservice \
+    .
