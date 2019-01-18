d3708f7c8e24546addc19364ff05f7db05ff556d
Adam Krieger <akrieger@obsglobal.com>
2019-01-18 10:33:05 -0600

    added boto3 to requirements so that version script works

--- orderservice/build.sh
+++ orderservice/build.sh
@@ -9,10 +9,10 @@ echo Building Executable
 
 GOOS=linux go build -a -o bin/orderservice main.go
 
-echo Copying Executable to Media Library
+# echo Copying Executable to Media Library
 
-# aws s3 cp --profile $1 ./bin/orderservice s3://bin-iacfreedom/orderservice/1.0.0/orderservice
-aws s3 cp ./bin/orderservice s3://bin-iacfreedom/orderservice/1.0.0/orderservice
+# # aws s3 cp --profile $1 ./bin/orderservice s3://bin-iacfreedom/orderservice/1.0.0/orderservice
+# aws s3 cp ./bin/orderservice s3://bin-iacfreedom/orderservice/1.0.0/orderservice
 
 # ls -al bin
 
